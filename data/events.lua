3a452e959c8b670536b395abc910f8060a7529fd
Bella MarTar <scilencerzleader@gmail.com>
2025-08-02 15:24:28 -0400

    Added basic scripting for a certain scene

---
+++ data/events.lua
@@ -0,0 +1,30 @@
+--[[
+////////////////////
+SURRENDER REMOVES INCOMING PROJECTILES
+////////////////////
+]]--
+
+function mods.multiverse.destroy_all_projectiles()
+    local projectiles = Hyperspace.App.world.space.projectiles
+    for i = 0, projectiles:size() - 1 do
+        local projectile = projectiles[i]
+        local projName = tostring(projectile.extend.name)
+        if not (projName == "" or projName == "nil" or projName == "PDS_SHOT") then
+            projectile:Kill()
+        end
+    end
+end
+
+script.on_game_event("FOURTH_WALL_BREAK_SPARE_2", false, mods.multiverse.destroy_all_projectiles)
+
+
+--[[
+////////////////////
+SHORT ERROR FROM CONTROLLER
+////////////////////
+]]--
+
+script.on_game_event("CANT_KILL_ME", false, function()
+    Hyperspace.ErrorMessage("Humorous attempt Kid, but this is my world.")
+    Hyperspace.setWindowTitle("Under My Control")
+end)
